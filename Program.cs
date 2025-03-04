f0631e5a718564eaa99b15e1260e7117d556805c
aysunfarhadli <aysunfarhadli4@gmail.com>
2025-03-04 12:04:01 -0800

    code added

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Birinci ədədi daxil edin (a): ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("İkinci ədədi daxil edin (b): ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        // Dəyişənlərin yerini dəyişirik
+        int temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine("Dəyişdirildikdən sonra:");
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+    }
+}
